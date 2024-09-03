78f70b35d94e6d26d9f78b7bcf856075927a263e
Arijit <saha65092@gmail.com>
2024-09-03 17:29:35 +0530

    added file uploader and input section

---
+++ src/routes/GridPattern.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const GridPattern = () => {
+    const columns = 41;
+    const rows = 11;
+    return (
+        <div className="absolute inset-0 flex bg-gray-100 dark:bg-neutral-900 flex-shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px">
+            {Array.from({ length: rows }).map((_, row) =>
+                Array.from({ length: columns }).map((_, col) => {
+                    const index = row * columns + col;
+                    return (
+                        <div
+                            key={`${col}-${row}`}
+                            className={`w-10 h-10 flex flex-shrink-0 rounded-[2px] ${index % 2 === 0
+                                ? "bg-gray-50 dark:bg-neutral-950"
+                                : "bg-gray-50 dark:bg-neutral-950 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]"
+                                }`}
+                        />
+                    );
+                })
+            )}
+        </div>
+    );
+};
+
+export default GridPattern;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Input from './FileUpload'
 function Home() {
     return (
         <div className=''>
             <Navbar />
+            <div className='w-1/2 m-auto my-14'>
+                <Input />
+            </div>
         </div>
     )
 }
